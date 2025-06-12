ab65fe2871e8823163e7998f898f986da42a6f48
Ahmed Khalid <a5medkhalidabdullah@gmail.com>
2025-06-12 16:17:27 +0200

    update

---
+++ 17-lists-sets-tuples.py
@@ -0,0 +1,7 @@
+#collection = single 'variable' used to store a multiple values 
+#   List  = [] ordered and changeable. Duplicated Ok 
+#   set   = {} unordered and immutable, but Add/Remove Ok. No dublicates
+#   Tuple = () ordered and unchangable. Duplicated is Ok and Faster.
+
+fruit = 'apple'
+
